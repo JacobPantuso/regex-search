8c9a1b123e7d748966967afbd840d090244bb148
Jacob Pantuso <jacobpantuso@outlook.com>
2023-01-12 17:32:12 -0500

    dist upload

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-echo 'www.jacobpantuso.ca' > CNAME
+#echo 'www.jacobpantuso.ca' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
